bc9f92d0d67f3031b609d4333b38979791020ed2
fortuneblycat <49164202+fortuneblycat@users.noreply.github.com>
2025-10-07 16:34:09 +0800

    ImageRec

--- RPi/settings.py
+++ RPi/settings.py
@@ -7,14 +7,11 @@ BAUD_RATE = 115200
 #API_IP = "192.168.0.100"
 #API_IP = "192.168.34.20" # gavpc-ntu
 #API_IP = "192.168.34.30" # yyleepc-ntu
-API_IP = "192.168.34.29" # yyleepc-ntu
+API_IP = "192.168.34.29" # dj pc
 #API_IP1 = "192.168.34.15" #jyeohpc-ntu
 API_IP1 = "192.168.34.12"
 API_PORT = 5003
 API_PORT1 = 5002
-
-# API_IP = "192.168.34.29" # dj
-# API_IP1 = "192.168.34.29" # dj
 
 # ROBOT SETTINGS
 OUTDOOR_BIG_TURN = False
